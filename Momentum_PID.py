2eb5010c413c7af0a8c6a304fd499bf6baadee5e
lp185123 <lp185123@ncr.com>
2022-05-15 12:04:43 +0100

    make xyz physics and fake 3d view

--- Momentum_PID.py
+++ Momentum_PID.py
@@ -6,9 +6,7 @@ import time
 from scipy import rand
 import _3DVisLabLib
 import random
-#good example
 
-#https://www.reddit.com/r/godot/comments/6pd58n/2d_rocket_simulation/
 
 @dataclass(order=True,repr=False)
 class _2d_Body:
@@ -23,16 +21,16 @@ class _2d_Body:
 
     def __post_init__(self):#need this for sorting
         object.__setattr__(self,'sort_index',self.Mass)
-        self.Force=np.array([0,0],dtype=float)
+        self.Force=np.array([0,0,0],dtype=float)
         self.position_2d=np.array(self.position_2d,dtype=float)
         self.ForceVector_2d=np.array(self.ForceVector_2d,dtype=float)
         self.velocity_2d=np.array(self.velocity_2d,dtype=float)
-        self.Radius=int(np.linalg.norm(self.ForceVector_2d))
+        self.Radius=int(np.linalg.norm(self.ForceVector_2d)+self.Mass)
         if self.Radius>30:
             self.Radius=30
-        if self.Radius<1:
-            self.Radius=1
-        self.Colour=(random.randint(50,255),random.randint(50,255),random.randint(50,255))
+        if self.Radius<2:
+            self.Radius=2
+        self.Colour=(random.randint(0,255),random.randint(0,255),random.randint(0,255))
 
     def SimulateAsPhysicsBody(self,InputObject,dT):
         '''reference to simulate using conservation of momentum, 
@@ -47,8 +45,8 @@ class _2d_Body:
         #create custom force vector depending on what we want to do
         ApplicationForceVector=DirectionToObject_unit * (1/(DistanceObject2me+100))**2#sort of equasion for gravity
         #compute force on self
-        self.Force=np.array([0,0],dtype=float)
-        self.Force+=ApplicationForceVector
+        #self.Force=np.array([0,0],dtype=float)
+        self.Force=ApplicationForceVector * InputObject.ForceVector_2d
         #apply newtons second law, compute accelerate
         Acceleration=self.Force/self.Mass
         #integrate once to get velocity
@@ -56,7 +54,6 @@ class _2d_Body:
         #FollowCam.velocity_2d=Acceleration*dt #use this for no inertia
         #integrate twice to get position
         self.position_2d+=self.velocity_2d* dT
-
         return
 
 
@@ -90,14 +87,14 @@ class TimeDiffObject:
 class SimViewBox():
     '''Display particles'''
     def __init__(self,Height,Width) -> None:
-        self.BaseImage=np.zeros([Height,Width],dtype="uint8")
+        self.BaseImage=np.zeros([Height,Width,3],dtype="uint8")
 
     def ResetImage(self):
-        self.BaseImage=np.zeros([self.BaseImage.shape[0],self.BaseImage.shape[1]],dtype="uint8")
+        self.BaseImage=np.zeros([self.BaseImage.shape[0],self.BaseImage.shape[1],3],dtype="uint8")
 
     def UpdateImage(self,Object):
         # Center coordinates
-        center_coordinates = tuple([int(x) for x in Object.position_2d] )
+        center_coordinates = tuple([int(x) for x in Object.position_2d[0:2]] )
         # Radius of circle
         radius = Object.Radius
         # Blue color in BGR
@@ -110,7 +107,7 @@ class SimViewBox():
         return image
 
 refPt = []#global object to handle recording mouse position
-GlobalMousePos=(0,0)#need to be global to handle capturing mouse on opencv UI
+GlobalMousePos=(250,250)#need to be global to handle capturing mouse on opencv UI
 ImgView_Resize=1.3#HD image doesnt fit on screen
 
 def FollowMouse(event, x, y, flags, param):
@@ -131,12 +128,13 @@ def main():
     cv2.namedWindow("img")
     cv2.setMouseCallback("img", FollowMouse)
     #create objects
-    for Indexer, CreateBody in enumerate(range (0,10)):
-        RandMass=random.random()
-        RandForce=random.random()*10
-        ListBodies.append(_2d_Body(str(Indexer),RandMass,[random.randint(100,400),random.randint(100,400)],[0,0],[RandForce,RandForce],0))
-    force_Gravity=_2d_Body("User",0.01,[250,250],[0,0],[30,30],0)
+    force_Gravity=_2d_Body("User",10.0,[250,250,0],[0,0,0],[20,20,20],0)
     ListBodies.append(force_Gravity)
+    for Indexer, CreateBody in enumerate(range (0,15)):
+        RandMass=random.randint(1,20)
+        RandForce=random.randint(1,20)
+        ListBodies.append(_2d_Body(str(Indexer),RandMass,[random.randint(100,400),random.randint(100,400),random.randint(100,400)],[0,0,0],[RandForce,RandForce,RandForce],0))
+    
 
 
     dTime=TimeDiffObject()
@@ -146,8 +144,8 @@ def main():
     global GlobalMousePos
     while True:
         #get time difference to keep it consistent
-        force_Gravity.position_2d=np.array(GlobalMousePos)
-        time.sleep(0.01)
+        force_Gravity.position_2d=np.array(list(GlobalMousePos) + [0])#only want X and Y but need a Z to satisfy logic
+        #time.sleep(0.01)
         dt=dTime.Get_dT()+10
         SimImage.ResetImage()
         for PhysBody in ListBodies:
@@ -155,67 +153,14 @@ def main():
 
         _3DVisLabLib.ImageViewer_Quickv2_UserControl(OutputSimImage,0,False,False)
 
-
         for PhysBody in ListBodies:
+            if all([PhysBody.Name=="User"]):
+                continue#don't process the user object
             for PhysBodyExt in ListBodies:
-                rules=[PhysBodyExt.Name!=PhysBody.Name,
-                PhysBody.Name!="User"]
+                rules=[PhysBodyExt.Name!=PhysBody.Name]
                 if all(rules):
                     PhysBody.SimulateAsPhysicsBody(PhysBodyExt,dt)
 
-
-
-def main2():
-    cv2.namedWindow("img")
-    cv2.setMouseCallback("img", FollowMouse)
-    #create objects
-    force_Gravity=_2d_Force("Gravity",[10,10])
-    FollowCam=_2d_Body("FollowCam",0.01,[250,250],[0,0],[0,0],0)
-    FollowCam=_2d_Body("FollowCam",0.01,[250,250],[0,0],[0,0],0)
-    dTime=TimeDiffObject()
-    SimImage=SimViewBox(500,500)
-
-    Counter=0
-    #global variable to store positon of users cursor in the opencv window
-    global GlobalMousePos
-    while True:
-        #get time difference to keep it consistent
-        force_Gravity.pos
-        time.sleep(0.01)
-        dt=dTime.Get_dT()+10
-
-        SimImage.ResetImage()
-        OutputSimImage= SimImage.UpdateImage(FollowCam.position_2d)
-        OutputSimImage= SimImage.UpdateImage(GlobalMousePos)
-        _3DVisLabLib.ImageViewer_Quickv2_UserControl(OutputSimImage,0,False,False)
-
-        #get directional vector from Camera to Ball
-        #subtract end point from start point
-        RelativeBallPos_2Cam=np.array(GlobalMousePos)-FollowCam.position_2d
-        #get distance from ball to camera
-        DistanceBall2Cam=np.linalg.norm(RelativeBallPos_2Cam)
-        #set to unit vector
-        DirectionToBall_unit=RelativeBallPos_2Cam/DistanceBall2Cam
-        #create custom force vector depending on what we want to do
-        ApplicationForceVector=DirectionToBall_unit * (1/(DistanceBall2Cam+100))**2
-        #update gravity
-        #force_Gravity.ForceVector_2d=#_2d_Force("Gravity",ApplicationForceVector)
-        
-        #compute force on object
-        FollowCam.Force=np.array([0,0],dtype=float)
-        FollowCam.Force+=ApplicationForceVector * force_Gravity.ForceVector_2d
-        #apply newtons second law, compute accelerate
-        Acceleration=FollowCam.Force/FollowCam.Mass
-        #integrate once to get velocity
-        FollowCam.velocity_2d+=Acceleration*dt
-        #FollowCam.velocity_2d=Acceleration*dt
-        #integrate twice to get position
-        FollowCam.position_2d+=FollowCam.velocity_2d* dt
-        
-        if int(FollowCam.position_2d[0]) not in range (0,SimImage.BaseImage.shape[0]):
-            FollowCam=_2d_Body("FollowCam",0.01,[250,250],[0,0],[0,0],0)
-        if int(FollowCam.position_2d[1]) not in range (0,SimImage.BaseImage.shape[1]):
-            FollowCam=_2d_Body("FollowCam",0.01,[250,250],[0,0],[0,0],0)
 
 if __name__ == "__main__":
     main()
